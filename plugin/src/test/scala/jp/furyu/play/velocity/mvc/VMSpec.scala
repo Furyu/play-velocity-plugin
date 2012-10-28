9013e7e90c15a2ce683953e9b20ec46268af5a72
Kuchitama <kiyo.kunihira@gmail.com>
2012-10-29 01:13:57 +0900

    Issues #9 modified to use user constructed instance of VelocityEngine

--- plugin/src/test/scala/jp/furyu/play/velocity/mvc/VMSpec.scala
+++ plugin/src/test/scala/jp/furyu/play/velocity/mvc/VMSpec.scala
@@ -4,6 +4,9 @@ import org.specs2.mutable._
 import play.api.test.Helpers._
 import play.api.test.FakeApplication
 
+import org.apache.velocity.app.VelocityEngine
+import org.specs2.execute.Failure
+
 class VMSpec extends Specification {
 
   private def runApp[T](block: => T): T = {
@@ -39,6 +42,31 @@ class VMSpec extends Specification {
     "evaluate scala field access" in runApp {
       case class Fuga(piyo: String)
       VM("test_template_for_scala_field.vm", Map("fuga" -> Fuga("__piyo__"))).body === """__piyo__"""
+    }
+
+    "DefaultProperty" in {
+      "return DefaultProperty instance" in {
+        val app = FakeApplication(
+          additionalPlugins = Seq("jp.furyu.play.velocity.VelocityPlugin"),
+          additionalConfiguration = Map("ehcacheplugin" -> "disabled") // without this, error [Cache play already exists] occurred.
+        )
+        running(app) {
+          DefaultProperty === app.plugin[jp.furyu.play.velocity.VelocityPlugin].get.DefaultProperty
+        }
+      }
+
+      "evaluate use argument engine" in runApp {
+        val property = DefaultProperty
+        property.setProperty("runtime.references.strict", "true")
+        val engine = new VelocityEngine(property)
+
+        try {
+          VM("test_template.vm", Map.empty, engine).body === """fuga_value"""
+          failure("VelocityEigin is not strict mode")
+        } catch {
+          case e: org.apache.velocity.exception.MethodInvocationException => e.getMessage must contain("fuga")
+        }
+      }
     }
 
   }
